d2fda824272330305ccc9a30b59ab734a8596665
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-10-03 14:36:41 +0900

    change main

--- Rust_Src/src/main.rs
+++ Rust_Src/src/main.rs
@@ -9,21 +9,16 @@ use router::Router;
 
 fn main(){
     let mut router = Router::new();
-    router.get("/", hello, "hello");
-    router.post("/", hello_prefix, "hello_prefix");
+    router.post("/", return_to_POST, "POST");
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:80").unwrap();
 
-    fn hello(_: &mut Request) -> IronResult<Response>{
-        Ok(Response::with((status::Ok, "Hello World")))
-    }
-
-    fn hello_prefix(req: &mut Request) -> IronResult<Response>{
+    fn return_to_POST(req: &mut Request) -> IronResult<Response>{
         let mut body = String::new();
-        req.body.read_to_string(&mut body)
-            .expect("Failed to read line");
+        req.body.read_to_string(&mut body).expect("error");
         
-        let res = "Hello ".to_string() + &body + &"!".to_string();
+        let res = "return to POST <<".to_string() + &body + &">>".to_string();
+        
         Ok(Response::with((status::Ok, res)))
     }
 }
